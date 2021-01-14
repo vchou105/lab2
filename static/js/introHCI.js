3b9d4fd73e9985fe513205dd82699a50d3bd9de1
Vicky <v2chou@ucsd.edu>
2021-01-15 03:33:28 +0800

    added Javascript!

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -4,6 +4,15 @@ $(document).ready(function() {
 
 function initializePage() {
     $('#testjs').click(function(e) {
-            $('.jumbotron h1').text("Javascript is connected");
-            });
+         $('.jumbotron h1').text("Javascript is connected");
+    });
+    $("a.thumbnail").click(projectClick);
+}
+
+function projectClick(e) {
+    // prevent the page from reloading     
+    e.preventDefault();
+    // In an event handler, $(this) refers to     
+    // the object that triggered the event     
+    $(this).css("background-color", "#7fff00");
 }
